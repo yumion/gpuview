8544c11f30d36dc7d9bbd0382991fa2d06bb66a8
XinNoil <jiankunwang@tju.edu.cn>
2022-10-17 21:37:16 +0800

    modi sth

--- gpuview/worker.py
+++ gpuview/worker.py
@@ -22,27 +22,30 @@ def get_gpustats(ttl, retry=3):
 def req_hosts(hosts, ttl):
     timeout_hosts = []
     for host in hosts:
-        try:
-            raw_resp = urlopen(host['url'] + '/gpustat', timeout = 1)
-            resp = raw_resp.read()
-            if type(resp) != str:
-                resp = resp.decode()
-            gpustat = json.loads(resp)
-            reset_flag(gpustat)
-            raw_resp.close()
-            if not gpustat or 'gpus' not in gpustat:
-                continue
-            client.set(host['name'], json.dumps(gpustat), ttl)
-        except Exception as e:
-            print('Error: %s getting gpustat from %s' %
-                (getattr(e, 'message', str(e)), host['url']))
+        if not req_host(host, ttl):
             timeout_hosts.append(host)
-            continue
     return timeout_hosts
+
+def req_host(host, ttl):
+    try:
+        raw_resp = urlopen(host['url'] + '/gpustat', timeout = 1)
+        resp = raw_resp.read()
+        if type(resp) != str:
+            resp = resp.decode()
+        gpustat = json.loads(resp)
+        reset_flag(gpustat)
+        raw_resp.close()
+        if gpustat is not None and 'gpus' in gpustat:
+            client.set(host['name'], json.dumps(gpustat), ttl)
+        return True
+    except Exception as e:
+        print('Error: %s getting gpustat from %s' %
+            (getattr(e, 'message', str(e)), host['url']))
+        return False
 
 def main():
     while True:
-        get_gpustats(10)
+        get_gpustats(16)
         time.sleep(8)
 
 if __name__ == '__main__':
